18288211fb26fba12a8b5a70ce5d466f14d0a967
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-11-13 16:50:59 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,8 +4,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
+    private final MemberRepository memberRepository = new MemoryMemberRepository();     //추상화에도 의존하고 구체화에도 의존하고있는것. dip위반
 
     @Override
     public void join(Member member) {
