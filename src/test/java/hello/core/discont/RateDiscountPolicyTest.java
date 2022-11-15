94ca3a531240fc3025755f0b7e712304d57c1690
kluedeugene <50358936+kluedeugene@users.noreply.github.com>
2022-11-15 17:25:08 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/core/discont/RateDiscountPolicyTest.java
@@ -0,0 +1,16 @@
+package hello.core.discont;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10% 할인이 적용되어야한다.")
+    void vip_0(){
+
+    }
+}
