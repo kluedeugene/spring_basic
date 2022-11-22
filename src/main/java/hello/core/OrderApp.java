9babaf88df0260bcf148405fb3a7f0986b443bee
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-11-22 22:20:33 +0900

    새로운 구조와 할인 정책 적용

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -22,9 +22,10 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
 
     }
 }
+
