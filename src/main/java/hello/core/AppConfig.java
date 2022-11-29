17b8b76e04b1160c5c4242708c7ca66be777c774
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-11-29 23:24:37 +0900

    appconfig -> spring 컨테이너 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
+    @Bean
         public MemberService    memberService(){    //어딘가에서 memberService를 사용하면 Memory멤버레포가 주입된다. 생성자주입이다.
             return new MemberServiceImpl(memberRepository());
         }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();        //  메모리 멤버 레포를 사용.  나중에 jdbc레포를 사용하고싶다면 이부분만 변경하면 된다.
     }
-
+    @Bean
     public OrderService orderService(){     //orderService를 조회하면 메모리멤버레포, 픽스디스카운트가 리턴
             return new OrderServiceImpl(memberRepository(),  discountPolicy());
         }
 
-        public DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
             return new RateDiscountPolicy();
 //            return new FixDiscountPolicy();
     }
@@ -32,3 +39,5 @@ public class AppConfig {
 
 //appconfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 //appconfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
+
+// configuration, bean -> 스프링 컨테이너에 등록하기위함.
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();        //memberservice 요청-> appconfig에서 memoryMemberRepo를 사용해서 impl 생성, 반환
-//        MemberService memberService = new MemberServiceImpl();        //기존에 직접 memberService 객체 생성..
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();        //memberservice 요청-> appconfig에서 memoryMemberRepo를 사용해서 impl 생성, 반환
+
+        //spring  적용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);        //1L = long  타입 지정
         memberService.join(member);
 
